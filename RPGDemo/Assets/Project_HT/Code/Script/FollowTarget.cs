16242201fe2cc340e76524eec31b478555388123
HHHXTCCCAQ <2507200459@qq.com>
2019-01-11 18:37:12 +0800

    2019-1-11_Updeate_v1

---
+++ RPGDemo/Assets/Project_HT/Code/Script/FollowTarget.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Wait for me, I don't want to let you down
+//love you into disease, but no medicine can.
+//Created By HeXiaoTao
+public class FollowTarget : MonoBehaviour {
+
+	// Use this for initialization
+    public  Vector3 Offset;
+    private Transform Player;
+    public float smoothing = 1;
+	void Start ()
+	{
+	    Player = GameObject.FindGameObjectWithTag("Player").transform.Find("Bip001");
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+	    //transform.position = Player.position + Offset;
+	    Vector3 targetPos = Player.position + Offset;
+	  transform.position= Vector3.Lerp(transform.position, targetPos, smoothing*Time.deltaTime);
+	}
+}
--- RPGDemo/Assets/Project_HT/Code/Script/Player/PlayerController.cs
+++ RPGDemo/Assets/Project_HT/Code/Script/Player/PlayerController.cs
@@ -9,13 +9,11 @@ public class PlayerController : MonoBehaviour
     public SetState setState;
     public bool isAttack = false;
     public float MoveSpeed = 5f;
-    private Transform PlayerTrans;
+    private Vector3 rigVelocity;
     private Rigidbody _rigidbody;
-    private bool isGround = true;
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
-        PlayerTrans = GetComponent<Transform>();
         Config.PlayerState = GetComponent<PlayerState>();
         setState = new SetState();
     }
@@ -24,53 +22,50 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         setState.Update();
-
-        Debug.Log(IsGround());
     }
 
     void FixedUpdate()
     {
         PlayerKeyboardController();
-        RecoverIdle();
     }
 
     private void PlayerKeyboardController()
     {
-        //if (Input.GetKey(KeyCode.W))
-        //{
-        //    if (PlayerTrans != null)
-        //        _rigidbody.velocity = new Vector3(MoveSpeed, _rigidbody.velocity.y, _rigidbody.velocity.z);
-        //        PlayerTrans.Translate(Vector3.forward * MoveSpeed * Time.deltaTime);
-        //}
-        //if (Input.GetKey(KeyCode.A))
-        //{
-        //    if (PlayerTrans != null)
-        //        PlayerTrans.Translate(Vector3.left * MoveSpeed * Time.deltaTime);
-        //}
-        //if (Input.GetKey(KeyCode.S))
-        //{
-        //    if (PlayerTrans != null)
-        //        PlayerTrans.Translate(Vector3.back * MoveSpeed * Time.deltaTime);
-        //}
-        //if (Input.GetKey(KeyCode.D))
-        //{
-        //    if (PlayerTrans != null)
-        //        PlayerTrans.Translate(Vector3.right * MoveSpeed * Time.deltaTime);
-        //}
-        float horizontal = Input.GetAxis("Horizontal"); //左右移动
-        float vertical = Input.GetAxis("Vertical");//上下移动
-        transform.Rotate(new Vector3(0, 1, 0) * horizontal);
-        _rigidbody.velocity = new Vector3(transform.forward.x*MoveSpeed * vertical,
-           transform.forward.y, transform.forward.z );
-
-
+        SetAnimation._instance.SetPlayerAnimation(Config.PlayerSpeed,
+            Mathf.Max(Mathf.Abs(_rigidbody.velocity.x), Mathf.Abs(_rigidbody.velocity.z)));
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        rigVelocity = _rigidbody.velocity;
+        if (Mathf.Abs(h) > 0.05f || Mathf.Abs(v) > 0.05f)
+        {
+            if (!isAttack)
+            {
+                Config.PlayerState.SetState(PlayerState.PLAYERSTATE.RUN);
+            }
+            else
+            {
+                Config.PlayerState.SetState(PlayerState.PLAYERSTATE.ATTACKRUN);
+            }
+            _rigidbody.velocity = new Vector3(v * MoveSpeed, rigVelocity.y, -h * MoveSpeed);
+            transform.rotation = Quaternion.LookRotation(new Vector3(v, 0, -h));
+        }
+        else
+        {
+            if (!isAttack)
+            {
+                Config.PlayerState.SetState(PlayerState.PLAYERSTATE.IDLE);
+            }
+            else
+            {
+                Config.PlayerState.SetState(PlayerState.PLAYERSTATE.ATTACKSTANDY);
+            }
+        }
         if (IsGround())
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 if (!isAttack)
                 {
-                    Debug.Log(1);
                     Config.PlayerState.SetState(PlayerState.PLAYERSTATE.JUMP);
                 }
                 else
@@ -97,40 +92,5 @@ public class PlayerController : MonoBehaviour
         }
         return isGround;
 
-    }
-
-    /// <summary>
-    /// 回到idle状态
-    /// </summary>
-    private void RecoverIdle()
-    {
-        SetAnimation._instance.SetPlayerAnimation(Config.PlayerSpeed, Mathf.Abs(_rigidbody.velocity.x));
-        //if (_rigidbody.velocity.magnitude > 0f)
-        //{
-        //    if (!isAttack)
-        //    {
-        //        Config.PlayerState.SetState(PlayerState.PLAYERSTATE.RUN);
-        //    }
-        //    else
-        //    {
-        //        Config.PlayerState.SetState(PlayerState.PLAYERSTATE.ATTACKRUN);
-        //    }
-        //}
-        //if (IsGround())
-        //{
-        //    //if (_rigidbody.velocity.magnitude <= 0f)
-        //    //{
-        //    //    if (!IsGround())
-        //    //        return;
-        //    //    if (!isAttack)
-        //    //    {
-        //    //        Config.PlayerState.SetState(PlayerState.PLAYERSTATE.IDLE);
-        //    //    }
-        //    //    else
-        //    //    {
-        //    //        Config.PlayerState.SetState(PlayerState.PLAYERSTATE.ATTACKSTANDY);
-        //    //    }
-        //    //}
-        //}
     }
 }
