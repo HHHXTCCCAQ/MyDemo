dd1fd4089527c28d9e348ab383abcf355a58e457
HHHXTCCCAQ <2507200459@qq.com>
2019-01-15 17:42:29 +0800

    2019-1-15_update_v1

---
+++ RPGDemo/Assets/Project_HT/Code/Script/Enemy/EnemyAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Wait for me, I don't want to let you down
+//love you into disease, but no medicine can.
+//Created By HeXiaoTao
+public class EnemyAnimation : MonoBehaviour {
+    private Animation enemyAnimation;
+	// Use this for initialization
+	void Start () {
+        enemyAnimation = GetComponent<Animation>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    #region Play Animation
+    public void PlayAnimation()
+    {
+
+    }
+    #endregion
+}
