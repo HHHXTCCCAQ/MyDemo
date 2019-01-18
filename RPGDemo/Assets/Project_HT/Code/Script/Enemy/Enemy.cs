71c619bc6f1f258eb239abc612d388e6260854af
HHHXTCCCAQ <2507200459@qq.com>
2019-01-18 17:57:01 +0800

    2019-1-18_update_v1

--- RPGDemo/Assets/Project_HT/Code/Script/Enemy/Enemy.cs
+++ RPGDemo/Assets/Project_HT/Code/Script/Enemy/Enemy.cs
@@ -1,18 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 //Wait for me, I don't want to let you down
 //love you into disease, but no medicine can.
 //Created By HeXiaoTao
-public class Enemy : MonoBehaviour {
+[RequireComponent(typeof(NavMeshAgent))]
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public float attackRang;
+    public float Hp = 100;
+    private float distanceToPlayer;
+    private Animation enemyAnimation;
+    private Transform taget;
+    private NavMeshAgent agent;
+    private bool isWalk;
+    // Use this for initialization
+    void Start()
+    {
+        enemyAnimation = GetComponent<Animation>();
+        taget = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = speed;
+        agent.SetDestination(taget.position);
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToPlayer = Vector3.Distance(transform.position, taget.position);
+    }
+
+    private void FixedUpdate()
+    {
+        EnemyAnimationPlayer();
+    }
+
+
+    public void EnemyAnimationPlayer()
+    {
+        if (Hp <= 0)
+            return;
+        if (distanceToPlayer > attackRang)
+        {
+            isWalk = true;
+            enemyAnimation.Play(Config.Run);
+            agent.SetDestination(taget.position);
+        }
+        else
+        {
+            //进入攻击状态
+            isWalk = false;
+            enemyAnimation.Stop(Config.Run); 
+        }
+    }
 }
--- RPGDemo/Assets/Project_HT/Code/Script/Enemy/EnemyAnimation.cs
+++ RPGDemo/Assets/Project_HT/Code/Script/Enemy/EnemyAnimation.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 //Wait for me, I don't want to let you down
 //love you into disease, but no medicine can.
 //Created By HeXiaoTao
-public class EnemyAnimation : MonoBehaviour {
-    private Animation enemyAnimation;
-	// Use this for initialization
-	void Start () {
+public class EnemyAnimation : MonoBehaviour
+{
+    protected Animation enemyAnimation;
+    // Use this for initialization
+    void Start()
+    {
         enemyAnimation = GetComponent<Animation>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     #region Play Animation
     public void PlayAnimation()
